4623618d6d9047bef6d383f251f5a077d7fb9710
zubairfazal <zubairfazal@icloud.com>
2017-05-12 10:14:53 +0100

    UCP Design
    Updated UCP design with tabs, and added jQuery. Added tabs for change
    email also, need to add the query for it next

--- ucp.php
+++ ucp.php
@@ -31,19 +31,49 @@ $sql = "UPDATE users SET password='$password1' WHERE username='$username1'";
 }
 ?>
 
+<div class="container"><h2>User Control Panel</h2></div>
+
+<div id="exTab2" class="container">
+<ul class="nav nav-tabs">
+			<li class="active">
+        <a  href="#1" data-toggle="tab">Overview</a>
+			</li>
+			<li><a href="#2" data-toggle="tab">Change Password</a>
+			</li>
+			<li><a href="#3" data-toggle="tab">Change Email</a>
+			</li>
+		</ul>
+
+			<div class="tab-content ">
+			  <div class="tab-pane active" id="1">
+          <h3>Welcome to the User Control Panel <b><?php echo $_SESSION['username'];?></b></h3>
+				</div>
+				<div class="tab-pane" id="2">
+          <form name="frmChange" role="form" class="form-signin" method="POST" action="">
+
+          <div class="form-group">
+
+            <label for="InputPassword2">New Password</label>
+            <input type="password" class="form-control" id="InputPassword2" placeholder="New Password" name="newPassword">
+             <label for="InputPassword3">Confirm New Password</label>
+            <input type="password" class="form-control" id="InputPassword3" placeholder="Confirm Password" name="confirmPassword">  </div>
+           <button class="btn btn-lrg btn-default btn-block" input type="submit" name="submit">Change it</button>
+				</div>
+        <div class="tab-pane" id="3">
+          <form name="frmChange" role="form" class="form-signin" method="POST" action="">
+
+          <div class="form-group">
+
+            <label for="InputEmail2">New Email</label>
+            <input type="email" class="form-control" id="InputEmai;2" placeholder="New Email" name="newEmail">
+             <label for="InputEmail3">Confirm New Email</label>
+            <input type="email" class="form-control" id="InputEmail3" placeholder="Confirm Email" name="confirmEmail">  </div>
+           <button class="btn btn-lrg btn-default btn-block" input type="submit" name="submit">Change it</button>
+				</div>
+			</div>
+  </div>
 
 
-Welcome to the User Control Panel <b><?php echo $_SESSION['username'];?></b>
-
-<form name="frmChange" role="form" class="form-signin" method="POST" action="">
-
-<div class="form-group">
-
-  <label for="InputPassword2">New Password</label>
-  <input type="password" class="form-control" id="InputPassword2" placeholder="New Password" name="newPassword">
-   <label for="InputPassword3">Confirm New Password</label>
-  <input type="password" class="form-control" id="InputPassword3" placeholder="Confirm Password" name="confirmPassword">  </div>
- <button class="btn btn-lrg btn-default btn-block" input type="submit" name="submit">Change it</button>
 
 
     </div>
